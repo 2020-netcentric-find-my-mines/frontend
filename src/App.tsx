f73033d49e41adda2477df8ad038b2ab5338ce34
Chanyut Yuvacharuskul <chanyut@wongnai.com>
2020-09-13 16:36:23 +0700

    set state as socket event input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import io from 'socket.io-client';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import './App.css';
+import { monitorEventLoopDelay } from 'perf_hooks'
+import React, {useState} from 'react'
+import io from 'socket.io-client'
+import './App.css'
 
 function App() {
-  let socket: any = null;
+
+  let [state, setState] = useState({
+    event: "",
+    id: "",
+    x: 0,
+    y: 0,
+  })
+
+  let socket: any = null
 
   console.log("socket")
 
   const handleConnect = () => {
-    socket = io('http://34.87.49.3:3000');
+    socket = io('http://34.87.49.3:3000')
   }
 
   const handleDisconnect = () => {
     console.log("lol not supported yet")
   }
 
+  const handleInputChange = (event: any) => {
+    event.persist()
+    setState((prevState) => {
+      return ({
+        ...prevState,
+        [event.target.name]: event.target.value,
+      })
+    })
+  }
+
   const handleSubmit = (event: any) => {
-    event.preventDefault();
-    console.log("HI");
-    console.log(event.target.name);
+    event.preventDefault()
+    console.log("HI")
+    console.log(event.target.name)
   }
 
   return (
@@ -29,14 +46,14 @@ function App() {
       <button onClick={handleConnect}>Connect</button>
       <button onClick={handleDisconnect}>Disconnect</button>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="event" placeholder="Socket Event" value=""/>
+        <input type="text" name="event" placeholder="Socket Event" value={state.event} onChange={handleInputChange} />
         <input type="text" name="id" placeholder="Game ID" />
         <input type="text" name="x" placeholder="X coordinate" />
         <input type="text" name="y" placeholder="Y coordinate" />
         <button>Submit</button>
       </form>
     </div>
-  );
+  )
 }
 
 export default App;
