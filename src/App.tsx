91c50c9c59a023c9287b25ba6b22dbeecf7ed7fd
Chanyut Yuvacharuskul <chanyut@wongnai.com>
2020-09-13 16:14:58 +0700

    solidify ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,38 @@ import React from 'react';
 import io from 'socket.io-client';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import {SocketEvent} from './socket-event';
 import './App.css';
 
 function App() {
+  let socket: any = null;
 
-  const handleClick = () => {
-    const socket = io('http://34.87.49.3:3000');
-    socket.emit(SocketEvent.CONNECTION, socket);
+  console.log("socket")
+
+  const handleConnect = () => {
+    socket = io('http://34.87.49.3:3000');
+  }
+
+  const handleDisconnect = () => {
+    console.log("lol not supported yet")
   }
 
   const handleSubmit = (event: any) => {
-    console.log(event.target);
+    event.preventDefault();
+    console.log("HI");
+    console.log(event.target.name);
   }
 
   return (
     <div className='App'>
       <h1>Minimum Viable Product for Find My Mines</h1>
-      <button onClick={handleClick}>Connect</button>
+      <button onClick={handleConnect}>Connect</button>
+      <button onClick={handleDisconnect}>Disconnect</button>
       <form onSubmit={handleSubmit}>
-        <TextField id="outlined-basic" label="Socket Event" />
-        <TextField id="outlined-basic" label="Game ID" />
-        <TextField id="outlined-basic" label="X coordinate" />
-        <TextField id="outlined-basic" label="Y coordinate" />
-        <Button variant="contained">Submit</Button>
+        <input type="text" name="event" placeholder="Socket Event" value=""/>
+        <input type="text" name="id" placeholder="Game ID" />
+        <input type="text" name="x" placeholder="X coordinate" />
+        <input type="text" name="y" placeholder="Y coordinate" />
+        <button>Submit</button>
       </form>
     </div>
   );
