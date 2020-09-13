bfa5834668af156f42dfa1ebe6dd468eb9f00c92
Chanyut Yuvacharuskul <chanyut@wongnai.com>
2020-09-13 14:58:27 +0700

    add socket connection and ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import io from 'socket.io-client';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {SocketEvent} from './socket-event';
 import './App.css';
 
 function App() {
+
+  const handleClick = () => {
+    const socket = io('http://34.87.49.3:3000');
+    socket.emit(SocketEvent.CONNECTION, socket);
+  }
+
+  const handleSubmit = (event: any) => {
+    console.log(event.target);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Minimum Viable Product for Find My Mines</h1>
+      <button onClick={handleClick}>Connect</button>
+      <form onSubmit={handleSubmit}>
+        <TextField id="outlined-basic" label="Socket Event" />
+        <TextField id="outlined-basic" label="Game ID" />
+        <TextField id="outlined-basic" label="X coordinate" />
+        <TextField id="outlined-basic" label="Y coordinate" />
+        <Button variant="contained">Submit</Button>
+      </form>
     </div>
   );
 }
