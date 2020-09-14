da7637f740d3613a8c438c06e8ba7a0f7995b61a
Chanyut Yuvacharuskul <chanyut@wongnai.com>
2020-09-14 19:22:14 +0700

    add components

---
+++ src/components/Table.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { SocketEvent } from '../socket-event';
+
+export default function Table(props: any) {
+
+    const handleSelectCoordinate = (event: any) => {
+        console.log("SELECT_COORDINATE", event.target.dataset.x, event.target.dataset.y)
+        props.emitEvent(SocketEvent.SELECT_COORDINATE, {
+            x: event.target.dataset.x,
+            y: event.target.dataset.y,
+        })
+    }
+
+    if (!props.visible) {
+        return <div>Start game to create table!</div>
+    }
+
+    let table: any = [];
+    for (let row: number = 0; row < +props.height; row++){
+        let columns = []
+
+        for (let column = 0; column < +props.width; column++) {
+            let cellID = `cell${row}-${column}`
+            let cell = props.coordinate[column * 6 + row]
+            let cellState
+            if (!cell.isSelected) {
+                cellState = "A"
+            } else if (cell.isBomb) {
+                cellState = "B"
+            } else {
+                cellState = "E"
+            }
+
+            columns.push(
+                <td key={cellID}>
+                    <button onClick={handleSelectCoordinate} data-x={column} data-y={row}>{cellState}</button>
+                </td>
+            )
+        }
+        table.push(<tr key={`row${row}`}>{columns}</tr>)
+    }
+
+    return (
+        <table>
+            <tbody>
+                {table}
+            </tbody>
+        </table>
+    )
+
+}
---
+++ src/components/Timer.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function Timer(props: any) {
+    //yes, it's that simple.
+    return(<h3>Timer: {props.tick}</h3>)
+}
