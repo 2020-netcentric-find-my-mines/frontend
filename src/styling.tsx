f0f67dac9dcf1650c001ab81b0be2362f3e158c9
Boom PT <boomeevee@icloud.com>
2020-09-18 16:50:13 +0700

    HomePage Design

---
+++ src/styling.tsx
@@ -0,0 +1,11 @@
+import { theme } from "@chakra-ui/core";
+
+const customTheme = {
+    ...theme,
+    colors: {
+      ...theme.colors,
+      accentColor: "#1a365d"
+    },
+  };
+
+  export default customTheme;
