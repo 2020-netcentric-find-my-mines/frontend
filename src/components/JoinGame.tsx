d2faec2ef598e57b6f4a328f1b4b4b24dfffe6dd
Boom PT <boomeevee@icloud.com>
2020-09-19 23:40:58 +0700

    fix redirect to #/play

--- src/components/JoinGame.tsx
+++ src/components/JoinGame.tsx
@@ -9,7 +9,7 @@ import {
   Heading,
   Input,
 } from "@chakra-ui/core";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { GameContext } from "../contexts/useGame";
 
 export default function JoinGame(props: any) {
@@ -29,51 +29,59 @@ export default function JoinGame(props: any) {
   }
 
   return (
-    <Flex
-      width="full"
-      height="100%"
-      align="center"
-      position="absolute"
-      justifyContent="center"
-      bg="gray.50"
-    >
-      <Box
-        p={10}
-        bg="white"
-        borderRadius={10}
-        boxShadow="0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)"
-      >
-        <Box textAlign="center">
-          <Heading>Join Game</Heading>
-        </Box>
-        <Box mt={4} textAlign="left" justifyItems="center">
-          <FormControl>
-            <FormLabel mb="2">Enter Game ID:</FormLabel>
-            <Input
-              type="text"
-              placeholder="Ex: XRTMK35"
-              variant="outline"
-              value={textfield}
-              onChange={handleChange}
-            />
-          </FormControl>
-          <Button
-            width="full"
-            mt={4}
-            variantColor={gameState.playerJoined === true ? "orange" : "teal"}
-            variant="solid"
-            onClick={gameState.playerJoined === true ? startGame : joinGame}
-            fontSize="sm"
+    <>
+      {gameState.started ? (
+        <Redirect to="/play" />
+      ) : (
+        <Flex
+          width="full"
+          height="100%"
+          align="center"
+          position="absolute"
+          justifyContent="center"
+          bg="gray.50"
+        >
+          <Box
+            p={10}
+            bg="white"
+            borderRadius={10}
+            boxShadow="0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)"
           >
-            {gameState.playerJoined === true ? "Start Game" : "Join Game"}
-          </Button>
-          <Link to="/game/create">
-            <Button width="full" mt="2" fontSize="sm" color="gray.600">
-              Create game
-            </Button>
-          </Link>
-        </Box>
-      </Box>
-    </Flex>
+            <Box textAlign="center">
+              <Heading>Join Game</Heading>
+            </Box>
+            <Box mt={4} textAlign="left" justifyItems="center">
+              <FormControl>
+                <FormLabel mb="2">Enter Game ID:</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="Ex: XRTMK35"
+                  variant="outline"
+                  value={textfield}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <Button
+                width="full"
+                mt={4}
+                variantColor={
+                  gameState.playerJoined === true ? "orange" : "teal"
+                }
+                variant="solid"
+                onClick={gameState.playerJoined === true ? startGame : joinGame}
+                fontSize="sm"
+              >
+                {gameState.playerJoined === true ? "Start Game" : "Join Game"}
+              </Button>
+              <Link to="/game/create">
+                <Button width="full" mt="2" fontSize="sm" color="gray.600">
+                  Create game
+                </Button>
+              </Link>
+            </Box>
+          </Box>
+        </Flex>
+      )}
+    </>
   );
 }
