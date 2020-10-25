6c95cd7f44dac4388675b0b88521fae17b9e1925
Jean <chanyut22@hotmail.com>
2020-10-25 12:40:02 +0700

    delete whitespace

---
+++ src/Firebase.tsx
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+import 'firebase/database'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBXZqXbdMLXf-lfNVaaRf7FTyS1khV_6yY",
+    authDomain: "findmymines.firebaseapp.com",
+    databaseURL: "https://findmymines.firebaseio.com",
+    projectId: "findmymines",
+    storageBucket: "findmymines.appspot.com",
+    messagingSenderId: "1082480748111",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
