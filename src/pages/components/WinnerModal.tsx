12973618c01bc6c87f672fbd03751bfff7918807
Patthanat Thanintantrakun <boomeevee@icloud.com>
2020-11-12 11:43:12 +0700

    Fix Winner Modal

--- src/pages/components/WinnerModal.tsx
+++ src/pages/components/WinnerModal.tsx
@@ -7,12 +7,13 @@ import {
   ModalFooter,
   ModalBody,
   Button,
+  Text,
 } from "@chakra-ui/core";
 import { GameContext } from "../../contexts/useGame";
 
 export default function WinnerModal() {
   const { gameState, gameDispatch } = useContext(GameContext);
-  
+
   function findWinner(): string {
     const winner = gameState.winner;
     if (winner.length === 1) {
@@ -31,14 +32,43 @@ export default function WinnerModal() {
         tiePlayers = tiePlayers + winner[i].name;
       }
       return (
-        "Tie between " +
-        tiePlayers +
-        " with a score of " +
-        winner[0].score
+        "Tie between " + tiePlayers + " with a score of " + winner[0].score
       );
     } else {
       return "";
     }
+  }
+
+  function findWinner2(): JSX.Element[] {
+    const player = gameState.players;
+    const winner = gameState.winner;
+    const winnerID = winner.map((winner) => winner.id);
+    let div: JSX.Element[] = [];
+
+    for (let i = 0; i < player.length; i++) {
+      if (winner.length === 1) {
+        div.push(
+          <Text
+            fontWeight={winnerID.includes(player[i].id) ? "bold" : "normal"}
+          >
+            {winnerID.includes(player[i].id) ? "🎉" : ""} {player[i].name}:{" "}
+            {player[i].score} {winnerID.includes(player[i].id) ? "🎉" : ""}{" "}
+            {winnerID.includes(player[i].id) ? "Winner" : ""}
+          </Text>
+        );
+      } else {
+        div.push(
+          <Text
+            fontWeight={winnerID.includes(player[i].id) ? "bold" : "normal"}
+          >
+            {winnerID.includes(player[i].id) ? "🎉" : ""} {player[i].name}:{" "}
+            {player[i].score} {winnerID.includes(player[i].id) ? "🎉" : ""}{" "}
+            {winnerID.includes(player[i].id) ? "Tie" : ""}
+          </Text>
+        );
+      }
+    }
+    return div;
   }
 
   return (
@@ -46,8 +76,13 @@ export default function WinnerModal() {
       <Modal isOpen={gameState.showWinnerModal}>
         <ModalOverlay />
         <ModalContent>
-  <ModalHeader>{gameState.winner.length === 1 ? "Congratulations" : "Tie"}!</ModalHeader>
-          <ModalBody>{findWinner()}</ModalBody>
+          <ModalHeader>
+            {gameState.winner.length === 1 ? "Congratulations" : "Tie"}!
+          </ModalHeader>
+          <ModalBody>
+            {findWinner()} <br /> <br />
+            {findWinner2()}
+          </ModalBody>
           <ModalFooter>
             <Button
               variantColor="blue"
