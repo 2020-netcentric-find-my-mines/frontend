12e6ad3a674416ecd328bbccd243594ca7cc2fad
Patthanat Thanintantrakun <boomeevee@icloud.com>
2020-11-12 12:36:46 +0700

    Update Header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -106,7 +106,7 @@ export default function Home() {
         boxShadow="0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)"
       >
         <Box textAlign="center">
-          <Heading>Mine Sweeper</Heading>
+          <Heading>Find My Mines</Heading>
         </Box>
 
         <Box my={4} textAlign="left" justifyItems="center">
